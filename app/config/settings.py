c24403407f4c3da31f5f72b8a82cf08ecd023079
Bayron <bramose@miumg.edu.gt>
2020-10-28 23:15:29 -0600

    Login terminado

--- app/config/settings.py
+++ app/config/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+
 import os
-from pathlib import Path
+
 import config.db as db
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -27,7 +27,6 @@ SECRET_KEY = '-cdhc=e04v^8u53f+^4gn0_(^j7us#tg@$(kfp1a%dwbi6o!ru'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -41,7 +40,9 @@ INSTALLED_APPS = [
     #libs
     'widget_tweaks',
     #aqui estan mis apps
-    'core.erp'
+    'core.erp',
+    'core.homepage',
+    'core.login',
 ]
 
 MIDDLEWARE = [
@@ -74,12 +75,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = db.POSTGRESQL
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -99,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -113,7 +111,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -122,3 +119,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_REDIRECT_URL = '/erp/category/list/'
--- app/core/erp/forms.py
+++ app/core/erp/forms.py
@@ -28,3 +28,22 @@ class CategoryForm(ModelForm):
                 }
             ),
         }
+
+    def save(self, commit=True):
+        data = {}
+        form = super()
+        try:
+            if form.is_valid():
+                form.save()
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        return data
+
+    def clean(self):
+        cleaned = super().clean()
+        if len(cleaned['name']) <= 50:
+            raise forms.ValidationError('Validacion xxx')
+            #self.add_error('name', 'Le faltan caracteres')
+        return cleaned
